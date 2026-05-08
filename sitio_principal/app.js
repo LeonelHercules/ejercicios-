3028dd60b7cdd7f8dd5b27c44a66919b084c266a
LeonelHercules <leonel_9217@hotmail.com>
2026-05-07 23:39:13 -0600

    Inicial

---
+++ sitio_principal/app.js
@@ -0,0 +1,22 @@
+document.getElementById("btn-saludar")
+.addEventListener("click", function() {
+    alert("Hola mundo desde javascript externo");
+});
+
+function validarFormulario() {
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    if (nombre === "" || correo === "" || mensaje === "") {
+        alert("Por favor, completa todos los campos.");
+        return false;
+    }
+    return true;
+}
+
+document.getElementById("formulario1").addEventListener("submit", function(event) {
+    if (!validarFormulario()) {
+        event.preventDefault();
+    }
+});
